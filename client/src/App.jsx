9d589135e1c5ca93bfbe11db7efa883c013d34ea
sefa <59339079+sefaalanur@users.noreply.github.com>
2024-06-26 00:20:07 +0200

    complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,10 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
